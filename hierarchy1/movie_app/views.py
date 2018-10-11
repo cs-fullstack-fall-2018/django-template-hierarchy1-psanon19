8e59a5ff3e0cc8ac05f79942734a37c446ca154c
Pelane Sanon <p_sanon@aol.com>
2018-10-11 16:08:33 -0500

    done

--- hierarchy1/movie_app/views.py
+++ hierarchy1/movie_app/views.py
@@ -4,6 +4,12 @@ from .models import MovieModel
 
 
 def index(request):
-    movie_model_list = MovieModel.objects.all
+    movie_model_list = MovieModel.objects.all()
     context = {'movie_model_list': movie_model_list}
     return render(request, 'movie_app/index.html', context)
+
+
+def dark(request):
+    movie_model_list = MovieModel.objects.all()
+    context = {'movie_model_list': movie_model_list}
+    return render(request, 'movie_app/newIndex.html', context)
